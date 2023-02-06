7da0ac625ff4b97641d7ed31d287f291a560d234
Bilal Moreno Ahmed <bilalmoreno92@gmail.com>
2023-02-06 18:09:59 +0100

    Ejemplo completo

--- lib/ui/views/validation_example/validation_example_view.dart
+++ lib/ui/views/validation_example/validation_example_view.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_arc/ui/views/validation_example/validation_example_viewmodel.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_hooks/stacked_hooks.dart';
 
-class ValidationExampleView extends StatelessWidget {
+class ValidationExampleView extends StackedHookView {
   const ValidationExampleView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget builder(BuildContext context, model) {
     return ViewModelBuilder<ValidationExampleViewModel>.reactive(
       builder: (context, model, child) => const Scaffold(),
       viewModelBuilder: () => ValidationExampleViewModel(),
     );
   }
+
+
 }
